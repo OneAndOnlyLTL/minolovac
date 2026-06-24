bec81c093cc29b8f3861d748e1d9974e2d27ac4e
OneAndOnlyLTL <thegrandmasterofforce@gmail.com>
2026-06-24 23:04:49 +0200

    dislajk za github

--- main.py
+++ main.py
@@ -1,290 +1,336 @@
 import pygame as pg
 import random
+
 pg.init()
 
 WIDTH = 800
-HEIGTH = 800
-SIZE = 28
-row, column = 24, 24
-tot_mines = 60
-left_mines = tot_mines
-STARTX, STARTY = (WIDTH - SIZE * column) // 2, (HEIGTH - SIZE * row) // 2 
-BACKGROUND = (100, 200, 200)
-dx, dy = [0, 0, -1, 1, -1, 1, -1, 1], [-1, 1, 0, 0, 1, 1, -1, -1]
-grid = [[0] * column for i in range(row)]
-mines = [[0] * column for i in range(row)]
-flags = [[0] * column for i in range(row)]
-known = [[0] * column for i in range(row)]
+HEIGHT = 800
+belo = (255, 255, 255)
+HUD = 80
 
-screen = pg.display.set_mode((WIDTH, HEIGTH))
-pg.display.set_caption("MINESWEEPER")
-fontBIG = pg.font.Font('freesansbold.ttf', 104)
-fontMED = pg.font.Font('freesansbold.ttf', 52)
-fontSMALL = pg.font.Font('freesansbold.ttf', 36)
-fontXS = pg.font.Font('freesansbold.ttf', 26)
-BIG = 104
-SMALL = 52
-win_txt = fontBIG.render('WIN!', False, 'black')
-loser = fontBIG.render('LOSER!', False, 'black')
-txt = fontMED.render('Click anywhere to play again.', False, 'black')
+nivoi = {
+    1: (8, 8, 15, 70),
+    2: (16, 16, 40, 40),
+    3: (24, 24, 75, 30),
+}
+
+red, kolona, brojmina, SIZE = nivoi[3]
+
+STARTX, STARTY = 0, 0
+
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("minolovac")
+#I was doing this for the first time, so I had to look up (and basically copy) this stuff
+font1 = pg.font.SysFont("segoeui", 96)
+font2 = pg.font.SysFont("segoeui", 48)
+font3 = pg.font.SysFont("segoeui", 22)
+
+win_txt = font1.render("pobeda", True, "black")
+loser = font1.render("ajde opet :<", True, "black")
+
 clock = pg.time.Clock()
-dt = 0
 
 pictures = []
-for i in range(12):
-    pictures.append(pg.transform.scale(pg.image.load('img/broj' + str(i) + '.png'), (SIZE, SIZE)))
+grid = []
+mines = []
+flags = []
+known = []
 
-def check(i, j, endi, endj) -> bool:
-    if i >= 0 and j >= 0 and i < endi and j < endj:
-        return True
-    return False
+playing = True
+running = True
+first = True
+brojostale = brojmina
+kraj = None   
 
-def location():
-    if not check(mouse['x'] - STARTX, mouse['y'] - STARTY, SIZE * column, SIZE * row):
+def rebuild_pictures():
+    global pictures
+    pictures = []
+    for i in range(12):
+        pictures.append(
+            pg.transform.scale(
+                pg.image.load("img/broj" + str(i) + ".png"),
+                (SIZE, SIZE)
+            )
+        )
+
+def reset_board():
+    global grid, mines, flags, known
+    grid = [[0] * kolona for _ in range(red)]
+    mines = [[0] * kolona for _ in range(red)]
+    flags = [[0] * kolona for _ in range(red)]
+    known = [[0] * kolona for _ in range(red)]
+
+def update_layout():
+    global STARTX, STARTY
+    STARTX = (WIDTH - SIZE * kolona) // 2
+    STARTY = (HEIGHT - SIZE * red) // 2 + HUD // 2
+
+def center_blit(text_surface, y):
+    x = (WIDTH - text_surface.get_width()) // 2
+    screen.blit(text_surface, (x, y))
+
+def check(r, c, R, C):
+    return 0 <= r < R and 0 <= c < C
+
+def location(pos):
+    x, y = pos
+    r = (y - STARTY) // SIZE
+    c = (x - STARTX) // SIZE
+    if not check(r, c, red, kolona):
         return -1, -1
-    i = (mouse['x'] - STARTX) // SIZE
-    j = (mouse['y'] - STARTY) // SIZE
-    return i, j
+    return r, c
 
-def restart():
-    global grid, mines, flags, known, playing, dt, tot_mines, column, row, pictures, running, STARTX, STARTY, SIZE
-    screen.fill(BACKGROUND)
-    tekst = fontSMALL.render("Kliknite broj od 1-3 za odgovarajucu tezinu.", False, 'black')
-    screen.blit(tekst, (20, 350))
-    pg.display.flip()
-    while True:
-        st = False
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                playing = False
-                running = False
-                st = True
-                return True
-            if event.type == pg.KEYUP and event.key == pg.K_1:
-                tot_mines = 15
-                row, column = 8, 8
-                SIZE = 35
-                st = True
-                break
-            if event.type == pg.KEYUP and event.key == pg.K_2:
-                tot_mines = 40
-                row, column = 16, 16
-                SIZE = 30
-                st = True
-                break
-            if event.type == pg.KEYUP and event.key == pg.K_3:
-                tot_mines = 75
-                row, column = 24, 24
-                SIZE = 22
-                st = True
-                break
-        
-        if st:
-            pictures = []
-            for i in range(12):
-                pictures.append(pg.transform.scale(pg.image.load('img/broj' + str(i) + '.png'), (SIZE, SIZE)))
-            STARTX, STARTY = (WIDTH - SIZE * column) // 2, (HEIGTH - SIZE * row) // 2 
-            break
-
-        dt = clock.tick(60) / 1000
-    grid = [[0] * column for i in range(row)]
-    mines = [[0] * column for i in range(row)]
-    flags = [[0] * column for i in range(row)]
-    known = [[0] * column for i in range(row)]
-    return False
-
-def drawGrid():
-    for i in range(row):
-        for j in range(column):
-            if known[i][j]:
-                screen.blit(pictures[grid[i][j]], (STARTX + SIZE * i, STARTY + SIZE * j))
-            else:
-                screen.blit(pictures[9], (STARTX + SIZE * i, STARTY + SIZE * j))
-            if mines[i][j] == 1 and known[i][j] == 1:
-                screen.blit(pictures[11], (STARTX + SIZE * i, STARTY + SIZE * j))
-            if flags[i][j] == 1:
-                screen.blit(pictures[10], (STARTX + SIZE * i, STARTY + SIZE * j))
-
-def generateMines():
+def generateMines(safe):
     global mines
-    for i in range(tot_mines):
-        x, y = random.randint(0, row - 1), random.randint(0, column - 1)
-        while mines[x][y] != 0:
-            x, y = random.randint(0, row - 1), random.randint(0, column - 1)
-        mines[x][y] = 1
+    safe = set(safe)
+
+    while True:
+        for r in range(red):
+            for c in range(kolona):
+                mines[r][c] = 0
+
+        candidates = [(r, c) for r in range(red) for c in range(kolona) if (r, c) not in safe]
+        if len(candidates) < brojmina:
+            raise ValueError("Too many mines for the current safe zone")
+
+        random.shuffle(candidates)
+        for r, c in candidates[:brojmina]:
+            mines[r][c] = 1
+
+        if any(mines[r][c] for r, c in safe):
+            continue
+
+        return
 
 def generateGrid():
-    global grid
-    for i in range(row):
-        for j in range(column):
-            if mines[i][j] == 1:
+    for r in range(red):
+        for c in range(kolona):
+            if mines[r][c]:
                 continue
+            count = 0
             for k in range(8):
-                ci, cj = i + dx[k], j + dy[k]
-                if check(ci, cj, row, column) and mines[ci][cj] == 1:
-                    grid[i][j] += 1
+                rr, cc = r + dx[k], c + dy[k]
+                if check(rr, cc, red, kolona) and mines[rr][cc]:
+                    count += 1
+            grid[r][c] = count
 
-def BFS(i, j):
-    global known
-    queue = []
-    if grid[i][j] == 0:
-        queue.append((i, j))
-    known[i][j] = 1
-    visited = [[0] * column for i in range(row)]
-    while len(queue):
-        x, y = queue[0][0], queue[0][1]
-        queue.pop(0) 
-        for k in range(8):
-            cx, cy = x + dx[k], y + dy[k]
-            if check(cx, cy, row, column) and (visited[cx][cy] == 0):
-                visited[cx][cy] = 1
-                if mines[i][j] == 1 or flags[i][j] == 1:
-                    continue
-                known[cx][cy] = 1
-                if grid[cx][cy] == 0:
-                    queue.append((cx, cy))
-
-def firstClick():
-    global first
-    if not mouse['left']:
-        return
-    if mouse['x'] - STARTX < 0 or mouse['y'] - STARTY < 0 or mouse['x'] - STARTX > SIZE * row or mouse['y'] - STARTX > SIZE * column:
-        return
-    i = (mouse['x'] - STARTX) // SIZE
-    j = (mouse['y'] - STARTY) // SIZE
-    mines[i][j] = -1
+def count_flags(r, c):
+    cnt = 0
     for k in range(8):
-        ci, cj = i + dy[k], j + dx[k]
-        if check(ci, cj, row, column):
-            mines[ci][cj] = -1
-    generateMines()
+        rr, cc = r + dx[k], c + dy[k]
+        if check(rr, cc, red, kolona) and flags[rr][cc]:
+            cnt += 1
+    return cnt
+
+def reveal(r, c):
+    q = [(r, c)]
+    vis = [[0] * kolona for _ in range(red)]
+    vis[r][c] = 1
+
+    while q:
+        x, y = q.pop(0)
+        if flags[x][y]:
+            continue
+        known[x][y] = 1
+
+        if grid[x][y] != 0:
+            continue
+
+        for k in range(8):
+            rr, cc = x + dx[k], y + dy[k]
+            if check(rr, cc, red, kolona) and not vis[rr][cc]:
+                vis[rr][cc] = 1
+                q.append((rr, cc))
+
+
+def firstClick(pos):
+    global first
+
+    r, c = location(pos)
+    if r == -1:
+        return
+
+    safe = {(r, c)}
+    for k in range(8):
+        rr, cc = r + dx[k], c + dy[k]
+        if check(rr, cc, red, kolona):
+            safe.add((rr, cc))
+
+    generateMines(safe)
     generateGrid()
-    BFS(i, j)
+    reveal(r, c)
     first = False
 
-def lose():
-    global running
-    for i in range(row):
-        for j in range(column):
-            known[i][j] = 1
-            flags[i][j] = 0
-    screen.fill(BACKGROUND)
-    drawGrid()
-    screen.blit(loser, (205, 45))
-    screen.blit(txt, (20, 670))
-    running = False
-
 def winChecker():
-    for i in range(row):
-        for j in range(column):
-            if  not (known[i][j] == 1 and mines[i][j] != 1) and not (known[i][j] == 0 and mines[i][j] == 1):
+    for r in range(red):
+        for c in range(kolona):
+            if not mines[r][c] and not known[r][c]:
                 return False
     return True
 
-def win():
-    global running
-    for i in range(row):
-        for j in range(column):
-            known[i][j] = 1
-            flags[i][j] = 0
-    screen.fill(BACKGROUND)
-    drawGrid()
-    screen.blit(win_txt, (270, 45))
-    screen.blit(txt, (20, 670))
+def lose():
+    global running, kraj
+    for r in range(red):
+        for c in range(kolona):
+            known[r][c] = 1
+    kraj = "lose"
     running = False
 
+def win():
+    global running, kraj
+    for r in range(red):
+        for c in range(kolona):
+            known[r][c] = 1
+    kraj = "win"
+    running = False
 
-def leftClick(i, j):
-    if mines[i][j] == 1 and flags[i][j] == 0:
-        lose()
-    elif flags[i][j] == 0:
-        if known[i][j] == 0:
-            BFS(i, j)
-        else:
+def leftClick(r, c):
+    if known[r][c]:
+        if grid[r][c] > 0 and count_flags(r, c) == grid[r][c]:
             for k in range(8):
-                ci, cj = i + dy[k], j + dx[k]
-                if check(ci, cj, row, column) and flags[ci][cj] == 0 and known[ci][cj] == 0:
-                    leftClick(ci, cj)
+                rr, cc = r + dx[k], c + dy[k]
+                if check(rr, cc, red, kolona) and not flags[rr][cc]:
+                    if mines[rr][cc]:
+                        lose()
+                        return
+                    reveal(rr, cc)
+        return
 
-def rightClick(i, j):
-    global left_mines
-    if known[i][j] == 0 and flags[i][j] == 0:
-        flags[i][j] = 1
-        left_mines -= 1
-    elif known[i][j] == 0 and flags[i][j] == 1:
-        flags[i][j] = 0
-        left_mines += 1
+    if flags[r][c]:
+        return
+
+    if mines[r][c]:
+        lose()
+    else:
+        reveal(r, c)
+
+def rightClick(r, c):
+    global brojostale
+    if known[r][c]:
+        return
+    if flags[r][c]:
+        flags[r][c] = 0
+        brojostale += 1
+    else:
+        flags[r][c] = 1
+        brojostale -= 1
+
+def drawGrid():
+    for r in range(red):
+        for c in range(kolona):
+            x = STARTX + c * SIZE
+            y = STARTY + r * SIZE
+
+            if known[r][c]:
+                screen.blit(pictures[grid[r][c]], (x, y))
+                if mines[r][c]:
+                    screen.blit(pictures[11], (x, y))
+            else:
+                screen.blit(pictures[9], (x, y))
+                if flags[r][c]:
+                    screen.blit(pictures[10], (x, y))
 
 
-playing = True
-while playing:      
+def restart():
+    global red, kolona, brojmina, SIZE, brojostale, first, kraj
+
+    screen.fill(belo)
+    t1 = font1.render("Izaberite težinu", True, 'black')
+    t2 = font2.render("1 : najlakša", True, 'black')
+    t3 = font2.render("2 : srednja", True, 'black')
+    t4 = font2.render("3 : najteža", True, 'black')
+
+    center_blit(t1, 200)
+    center_blit(t2, 320)
+    center_blit(t3, 390)
+    center_blit(t4, 460)
+    pg.display.flip()
+
+    while True:
+        clock.tick(60)
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                return True
+            if e.type == pg.KEYUP:
+                if e.key in [pg.K_1, pg.K_2, pg.K_3]:
+                    red, kolona, brojmina, SIZE = nivoi[int(e.unicode)]
+                    rebuild_pictures()
+                    update_layout()
+                    reset_board()
+                    brojostale = brojmina
+                    first = True
+                    kraj = None
+                    return False
+
+rebuild_pictures()
+update_layout()
+reset_board()
+
+while playing:
     running = True
     first = True
+    kraj = None
+
     if restart():
         break
+
+    brojostale = brojmina
     dt = 0
-    left_mines = tot_mines
+
     while running:
-        screen.fill(BACKGROUND)
-        drawGrid()
-        second_txt = fontXS.render('Sekunde:' + str(int(dt)), False, 'black')
-        screen.blit(second_txt, (0, 0))
-        mines_txt = fontXS.render('Ostalo mina: ' + str(left_mines), False, 'black')
-        screen.blit(mines_txt, (0, 30))
-        mouse = {
-            "x": pg.mouse.get_pos()[0],
-            "y": pg.mouse.get_pos()[1],
-            "left": pg.mouse.get_pressed()[0],
-            "right": pg.mouse.get_pressed()[2]
-        }
-
-        while first:
-            screen.fill(BACKGROUND)
-            drawGrid()
-            mouse = {
-                "x": pg.mouse.get_pos()[0],
-                "y": pg.mouse.get_pos()[1],
-                "left": pg.mouse.get_pressed()[0],
-                "right": pg.mouse.get_pressed()[2]
-            }
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    playing = False
-                    running = False
-                    first = False
-                if event.type == pg.MOUSEBUTTONUP:
-                    firstClick()
-            dt = clock.tick(60) / 1000
-            pg.display.flip()
-
-        
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-                playing = False
-            if event.type == pg.MOUSEBUTTONUP:
-                if mouse['left']:
-                    i, j = location()
-                    if i != -1:
-                        leftClick(i, j)
-                elif mouse['right']:
-                    i, j = location()
-                    if i != -1:
-                        rightClick(i, j)
-        if winChecker():
-            win()
         dt += clock.tick(60) / 1000
-        pg.display.flip()
-    while True:
-        st = False
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
                 playing = False
-                st = True
-                break
-            if event.type == pg.MOUSEBUTTONUP:
-                st = True
-                break
-        if st:
-            break
-        dt = clock.tick(60) / 1000
+                running = False
+            if e.type == pg.MOUSEBUTTONUP:
+                if first and e.button == 1:
+                    firstClick(e.pos)
+                else:
+                    r, c = location(e.pos)
+                    if r != -1:
+                        if e.button == 1:
+                            leftClick(r, c)
+                        if e.button == 3:
+                            rightClick(r, c)
+
+        if kraj is None and winChecker():
+            win()
+        screen.fill(belo)
+        pg.draw.rect(screen, (220, 220, 220), (0, 0, WIDTH, HUD))
+
+        t1 = font3.render(f"sekunde: {int(dt)}", False, "black")
+        t2 = font3.render(f"mine: {brojostale}", False, "black")
+        screen.blit(t1, (20, 20))
+        screen.blit(t2, (20, 45))
+
+        drawGrid()
+        pg.display.flip()
+    screen.fill(belo)
+    pg.draw.rect(screen, (220, 220, 220), (0, 0, WIDTH, HUD))
+
+    t1 = font3.render(f"sekunde: {int(dt)}", False, "black")
+    t2 = font3.render(f"mine: {brojostale}", False, "black")
+    screen.blit(t1, (20, 20))
+    screen.blit(t2, (20, 45))
+
+    drawGrid()
+
+    if kraj == "win":
+        center_blit(win_txt, 5)
+    else:
+        center_blit(loser, 5)
+
+    pg.display.flip()
+
+    waiting = True
+    while waiting:
+        clock.tick(60)
+        for e in pg.event.get():
+            if e.type in (pg.QUIT, pg.MOUSEBUTTONUP, pg.KEYUP):
+                waiting = False
+
+pg.quit()
